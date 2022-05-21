06b90206854cfe07c01f02debb9eafb874a63aab
seyioo <81486074+seyiooo@users.noreply.github.com>
2022-05-21 14:42:07 +0200

    Create ready.js

---
+++ src/_listeners/Client/ready.js
@@ -0,0 +1,49 @@
+const Event = require('../../_managers/structures/Event');
+const colors = require('colors');
+
+class ReadyEvent extends Event {
+    constructor(client) {
+        super(client, {
+            name: 'ready'
+        });
+    };
+    run () {
+        const statuses = [
+            `${this.client.guilds.cache.size.toLocaleString()} servers`,
+            `${this.client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b).toLocaleString()} users`,
+            `${this.client.commands.size.toLocaleString()} slash commandes`
+        ];
+
+        let i = 0;
+        setInterval(() => {
+            this.client.user.setActivity(statuses[i], { type: 'WATCHING' });
+            i = ++i % statuses.length;
+        }, 15000);
+
+        switch (process.argv[2]) {
+            case 'eval':
+                console.log('[Disbot]'.blue + ' Starting as eval mode.\n');
+
+                this.client.loadAtomEval();
+            break;
+            case 'load':
+                console.log('[Disbot]'.blue + ' Starting as load mode.\n');
+
+                this.client.application.commands.set(this.client.commands.map((cmd) => cmd.config));
+
+                console.log('[Disbot]'.blue + ` ${this.client.commands.size} commands has been loaded.\n`);
+
+                console.log('[Disbot]'.blue + ' Loaded in ' + `v${this.client.config.utils.version}`.blue + '. Informations:' + '\n➜ ID: ' + `${this.client.user.id}`.bgWhite.black + '\n➜ Servers: ' + `${this.client.guilds.cache.size.toLocaleString()}`.blue + '\n➜ Users: ' + `${this.client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b).toLocaleString()}`.blue + '\n➜ Commands: ' + `${this.client.commands.size}`.blue + '\n➜ Events: ' + `${this.client._eventsCount}`.blue + '\n');
+            break;
+            case undefined:
+                this.client.guilds.cache.get(this.client.config.utils.guild).commands.set(this.client.commands.map((cmd) => cmd.config));
+
+                console.log('[Disbot]'.blue + ` ${this.client.commands.size} commands has been loaded.\n`);
+
+                console.log('[Disbot]'.blue + ' Loaded in ' + `v${this.client.config.utils.version}`.blue + '. Informations:' + '\n➜ ID: ' + `${this.client.user.id}`.bgWhite.black + '\n➜ Servers: ' + `${this.client.guilds.cache.size.toLocaleString()}`.blue + '\n➜ Users: ' + `${this.client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b).toLocaleString()}`.blue + '\n➜ Commands: ' + `${this.client.commands.size}`.blue + '\n➜ Events: ' + `${this.client._eventsCount}`.blue + '\n');
+            break;
+        };
+    };
+};
+
+module.exports = ReadyEvent;
