a3aa22f4efcca8d29fd206ce71603bbf24e45d17
seyioo <81486074+seyiooo@users.noreply.github.com>
2022-05-21 14:42:58 +0200

    Create interactionCreate.js

---
+++ src/_listeners/Guild/interactionCreate.js
@@ -0,0 +1,23 @@
+const Event = require('../../_managers/structures/Event');
+
+class InteractionCreateEvent extends Event {
+    constructor(client) {
+        super(client, {
+            name: 'interactionCreate'
+        });
+    };
+    run (interaction) {
+        if (interaction.isCommand()) {
+            const command = this.client.commands.get(interaction.commandName);
+
+            if (!command) {
+                interaction.reply('Sorry, an error occured.');
+                console.log('[Disbot]'.bgWhite.black + ` Command ${interaction.commandName} doesn\'t exist.`);
+            };
+
+            command.run(interaction);
+        };
+    };
+};
+
+module.exports = InteractionCreateEvent;
