775213fa682dd07b5f12a9fd4a836906fed51121
seyioo <81486074+seyiooo@users.noreply.github.com>
2022-05-21 15:26:20 +0200

    Create ping.js

---
+++ src/_interactions/Commands/Information/ping.js
@@ -0,0 +1,25 @@
+const Command = require('../../../_managers/structures/Command');
+const { MessageEmbed } = require('discord.js');
+
+class PingCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ping',
+            description: 'Permet d\'avoir la latence du bot.',
+            category: 'info',
+            meperms: ['SEND_MESSAGES']
+        });
+    };
+    async run (interaction) {
+        interaction.reply({
+            embeds: [
+                new MessageEmbed()
+                .setTitle('Ping')
+                .setDescription(`Ping: ${this.client.ws.ping}ms`)
+                .setColor(interaction.guild)
+            ]
+        });
+    };
+};
+
+module.exports = PingCommand;
