1f9765a919faa55065b3c0c854e01e85432e8e0a
seyioo <81486074+seyiooo@users.noreply.github.com>
2022-05-21 15:44:48 +0200

    Update interactionCreate.js

--- src/_listeners/Guild/interactionCreate.js
+++ src/_listeners/Guild/interactionCreate.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const Event = require('../../_managers/structures/Event');
 
 class InteractionCreateEvent extends Event {
@@ -7,13 +8,34 @@ class InteractionCreateEvent extends Event {
         });
     };
     run (interaction) {
+        if (!interaction.guild || interaction.user.bot) return;
+
         if (interaction.isCommand()) {
             const command = this.client.commands.get(interaction.commandName);
 
-            if (!command) {
-                interaction.reply('Sorry, an error occured.');
-                console.log('[Disbot]'.blue + ` Command ${interaction.commandName} doesn\'t exist.`);
-            };
+            if (!command) return interaction.reply({
+                embeds: [
+                    new MessageEmbed()
+                    .setTitle('Error')
+                    .setDescription('Sorry, an error occured.')
+                ]
+            });
+
+            if (!interaction.member.permissions.has(command.config.perms)) return interaction.reply({
+                embeds: [
+                    new MessageEmbed()
+                    .setTitle('Error')
+                    .setDescription(`Sorry, you don\'t have permission to run this command.\n${command.config.perms.map((perm) => `\`${perm}\``).join(', ')}`)
+                ]
+            });
+
+            if (!interaction.guild.me.permissions.has(command.config.meperms)) return interaction.reply({
+                embeds: [
+                    new MessageEmbed()
+                    .setTitle('Error')
+                    .setDescription(`Sorry, I don\'t have permission to run this command.\n${command.config.meperms.map((perm) => `\`${perm}\``).join(', ')}`)
+                ]
+            });
 
             command.run(interaction);
         };
